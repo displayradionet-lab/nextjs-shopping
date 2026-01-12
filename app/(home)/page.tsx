dae0a0ed6a7f97040457c1dc16f149078ff22c59
displayradio.net-lab <displayradio.net@gmail.com>
2026-01-12 04:13:14 -0500

    best sellers

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -55,6 +55,7 @@ export default async function Page() {
   ];
 
   const todaysDeals = await getProductsByTag({ tag: 'todays-deal' });
+  const bestSellingProducts = await getProductsByTag({ tag: 'best-seller' });
   
   return (
     <>
@@ -67,6 +68,16 @@ export default async function Page() {
           <ProductSlider title="Today's Deals" products={todaysDeals} />
         </CardContent>
       </Card>
+
+           <Card className='w-full rounded-none'>
+          <CardContent className='p-4 items-center gap-3'>
+            <ProductSlider
+              title="Best Selling Products"
+              products={bestSellingProducts}
+              hideDetails
+            />
+          </CardContent>
+        </Card>
     </div>
     </>
   )
